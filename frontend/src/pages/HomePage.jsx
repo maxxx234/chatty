aecbc8738f59b385dfbf2d0e75666e7b720ad046
maxxx234 <meanzzz38@gmail.com>
2025-03-09 15:13:06 +0530

    Fix missing Sidebar.jsx file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useChatStore } from "../store/useChatStore"
-import Sidebar from "../components/sidebar";
+
+import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer"
 import NoChatSelected from "../components/NotChatSelected";
 
