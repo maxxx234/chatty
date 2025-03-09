b566e8efbe59832c88d53e579d02cdaff6096f62
maxxx234 <meanzzz38@gmail.com>
2025-03-09 14:59:29 +0530

    Fix missing axio.js file

--- frontend/src/lib/axio.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-// axios is a js library that is used to amk requests to servers  from webbrowsers or node js
-export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ?  "http://localhost:5001/api": "/api",
-     withCredentials: true,
-     
-}) 
